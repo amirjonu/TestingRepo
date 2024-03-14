0c68a3eec9ee26380bf3b190ede1c590654bc1f1
Amirjon Ulmasov <amirjonu@nycstudents.net>
2024-03-14 10:43:53 -0400

    php stuff

--- theme/pricing.php
+++ theme/pricing.php
@@ -5,13 +5,6 @@
  // GITHUB: https://github.com/themefisher/
 -->
 
-<!--
- // WEBSITE: https://themefisher.com
- // TWITTER: https://twitter.com/themefisher
- // FACEBOOK: https://www.facebook.com/themefisher
- // GITHUB: https://github.com/themefisher/
--->
-
 <!-- 
 THEME: Constra - Construction Html5 Template
 VERSION: 1.0.0
@@ -25,6 +18,9 @@ WEBSITE: https://themefisher.com
 TWITTER: https://twitter.com/themefisher
 FACEBOOK: https://www.facebook.com/themefisher
 -->
+
+
+<?php header("Content-Type: text/html; charset=UTF-8"); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -266,6 +262,86 @@ FACEBOOK: https://www.facebook.com/themefisher
         <li>Contact us for furthur inquiries</li>
       </ul>
   </div><!-- Intro box end -->
+
+  <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mt-4">
+                <div class="card-header">
+                    <h4>Input Your Address for Pricing Estimate</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-7">
+
+                            <form action="" method="GET">
+                                <div class="input-group mb-3">
+                                    <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>"" class="form-control" placeholder="Search data">
+                                    <button type="submit" class="btn btn-primary">Search</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="card mt-4">
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                                $con = mysqli_connect("localhost","root","","Practice");
+
+                                if(isset($_GET['search']))
+                                {
+                                    $filtervalues = $_GET['search'];
+                                    $query = "SELECT * FROM users WHERE CONCAT(firstname,lastname,email) LIKE '%$filtervalues%' ";
+                                    $query_run = mysqli_query($con, $query);
+
+                                    if(mysqli_num_rows($query_run) > 0)
+                                    {
+                                        foreach($query_run as $items)
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td><?= $items['id']; ?></td>
+                                                <td><?= $items['firstname']; ?></td>
+                                                <td><?= $items['lastname']; ?></td>
+                                                <td><?= $items['email']; ?></td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ?>
+                                            <tr>
+                                                <td colspan="4">No Record Found</td>
+                                            </tr>
+                                        <?php
+                                    }
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
   </div><!-- Conatiner end -->
 </section><!-- Main container end -->
